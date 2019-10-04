fcf9e2e9087c01d14ac57d2612486ae6d117104d
caenrigen <31376402+caenrigen@users.noreply.github.com>
2019-10-04 21:25:09 +0200

    fixed stuff-only sections

--- src/main/python/EventsBuilder.py
+++ src/main/python/EventsBuilder.py
@@ -1,9 +1,11 @@
 
 from datetime import datetime, timedelta
-import os, xml.etree.ElementTree as ET
+import os
+import xml.etree.ElementTree as ET
 import logging
 
 log = logging.getLogger(__name__)
+
 
 class EventsBuilder(object):
 	def __init__(self, createAllDayEvents=True, eventDurationH=1, deadlineDurationH=6, defaultReminders=False, emailReminderBeforeH=24):
@@ -73,15 +75,15 @@ class EventsBuilder(object):
 		attrib = courseXML.getroot().attrib
 
 		titleCourseStart = {'en': 'Course start', 'pt': 'Início do curso'}
-		titleCourseEnd =   {'en': 'Course end', 'pt': 'Fim do curso'}
+		titleCourseEnd = {'en': 'Course end', 'pt': 'Fim do curso'}
 		titleEnrollStart = {'en': 'Enrollment start', 'pt': 'Abertura inscrições'}
-		titleEnrollEnd =   {'en': 'Enrollment end', 'pt': 'Fecho inscrições'}
+		titleEnrollEnd = {'en': 'Enrollment end', 'pt': 'Fecho inscrições'}
 
 		globalCourseEvents = [
-		[ titleCourseStart[self.lang] , attrib.get('start').strip('\"')],
-		[ titleCourseEnd[self.lang] , attrib.get('end').strip('\"')],
-		[ titleEnrollStart[self.lang] , attrib.get('enrollment_start').strip('\"')],
-		[ titleEnrollEnd[self.lang] , attrib.get('enrollment_end').strip('\"')]
+		[titleCourseStart[self.lang], attrib.get('start').strip('\"')],
+		[titleCourseEnd[self.lang], attrib.get('end').strip('\"')],
+		[titleEnrollStart[self.lang], attrib.get('enrollment_start').strip('\"')],
+		[titleEnrollEnd[self.lang], attrib.get('enrollment_end').strip('\"')]
 		]
 
 		for globalEvent in globalCourseEvents:
@@ -93,16 +95,17 @@ class EventsBuilder(object):
 		for file in os.listdir():
 			if file.endswith('.xml'):
 				attrib = ET.parse(file).getroot().attrib
-				start = attrib.get('start')
-				title = attrib.get('display_name')
-				if start:
-					start = start.strip('\"')
-					title = titlePrefix[self.lang] + ': ' + title.strip('\"')
-					self.appendEvent(title, start, self.addHours(start, self.eventDurationH))
-					if self.createAllDayEvents:
-						self.appendAllDayEvent(title, start, start)
-				else:
-					log.warning('No start date found for chapter: "'+ title + '"" in file: ' + file + '.\n    You might need to change the date in Studio to a diffent one and back again.')
+				if attrib.get('visible_to_staff_only') != 'true':
+					start = attrib.get('start')
+					title = attrib.get('display_name')
+					if start:
+						start = start.strip('\"')
+						title = titlePrefix[self.lang] + ': ' + title.strip('\"')
+						self.appendEvent(title, start, self.addHours(start, self.eventDurationH))
+						if self.createAllDayEvents:
+							self.appendAllDayEvent(title, start, start)
+					else:
+						log.warning('No start date found for chapter: "'+ title + '"" in file: ' + file + '.\n    You might need to change the date in Studio to a diffent one and back again.')
 
 	def appendDeadlineEvents(self):
 		# Deadlines for subsections
@@ -111,20 +114,21 @@ class EventsBuilder(object):
 		for file in os.listdir():
 			if file.endswith('.xml'):
 				attrib = ET.parse(file).getroot().attrib
-				due = attrib.get('due')
-				if due:
-					due = due.strip('\"')
-					title = titlePrefix[self.lang] + ': ' + attrib.get('display_name').strip('\"')
-					self.appendEvent(title, self.addHours(due, - self.deadlineDurationH), due)
-					if self.createAllDayEvents:
-						self.appendAllDayEvent(title, due, due)
+				if attrib.get('visible_to_staff_only') != 'true':
+					due = attrib.get('due')
+					if due:
+						due = due.strip('\"')
+						title = titlePrefix[self.lang] + ': ' + attrib.get('display_name').strip('\"')
+						self.appendEvent(title, self.addHours(due, - self.deadlineDurationH), due)
+						if self.createAllDayEvents:
+							self.appendAllDayEvent(title, due, due)
 
 	def appendPeerReviewEvents(self):
 		# Peer Review
 		titlePrefixSubStart = {'en': 'Response submission opened', 'pt': 'Início submissão de respostas'}
-		titlePrefixSubDue   = {'en': 'Response submission deadline', 'pt': 'Fim de prazo submissão de respostas'}
-		titlePrefixPAstart  = {'en': 'Peer assessment start', 'pt': 'Início avaliação dos pares'}
-		titlePrefixPAdue    = {'en': 'Peer assessment deadline', 'pt': 'Fim de prazo avaliação dos pares'}
+		titlePrefixSubDue = {'en': 'Response submission deadline', 'pt': 'Fim de prazo submissão de respostas'}
+		titlePrefixPAstart = {'en': 'Peer assessment start', 'pt': 'Início avaliação dos pares'}
+		titlePrefixPAdue = {'en': 'Peer assessment deadline', 'pt': 'Fim de prazo avaliação dos pares'}
 		os.chdir(self.verticalDir)
 		for file in os.listdir():
 			if file.endswith('.xml'):
